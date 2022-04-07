988a8b6b4057e69c78ff74d7b9f9427fbc0cc7fd
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2022-04-07 13:47:20 -0300

    Add files via upload

---
+++ Logo windows 2.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+colors = ['#f5480f', '#2aad21', '#288cf7', '#ffcc00']
+
+pos = [(0,0), (100,0), (0, -100), (100, -100)]
+
+for (x,y), color in zip(pos,colors):
+    up()
+    goto(x,y)
+    down()
+    fillcolor(color)
+    begin_fill()
+    for i in range(4):
+        forward(90)
+        right(90)
+    end_fill()
+
+exitonclick()
+
+
+
+
+
